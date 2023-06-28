b4221e1447f3bde50c8e98c1556ef803a822c734
mahaidong <mahaidong@caad.xyz>
2023-06-28 11:43:57 +0800

    emcc

--- src/101.cpp
+++ src/101.cpp
@@ -1,42 +1,20 @@
-#ifdef __EMSCRIPTEN__
 #include <emscripten.h>
-#endif
-
-#include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengles2.h>
 
-bool quit = false;
-
-void mainLoop()
-{
-    SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-        if (event.type == SDL_QUIT)
-        {
-            quit = true;
-        }
-    }
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    SDL_GL_SwapWindow(window);
-}
-
-// gcc -o 101 101.c -lSDL2 -lGLESv2
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Hello Triangle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE| SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("Hello Triangle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
-    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     GLfloat vertices[] = {
         0.0f, 0.5f,
         -0.5f, -0.5f,
-        0.5f, -0.5f};
+        0.5f, -0.5f
+    };
 
     GLuint vbo;
     glGenBuffers(1, &vbo);
@@ -55,7 +33,7 @@ int main(int argc, char *argv[])
 
     const char *fragmentShaderSource =
         "void main() {\n"
-        "   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "   gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
         "}\n";
 
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -73,13 +51,21 @@ int main(int argc, char *argv[])
     glEnableVertexAttribArray(posAttrib);
     glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE,
                           2 * sizeof(GLfloat), 0);
-#ifdef __EMSCRIPTEN__
-    int fps = 0; // Use browser's requestAnimationFrame
-    emscripten_set_main_loop_arg(mainLoop, NULL, fps, !quit);
-#else
-    while (!quit)
-        mainLoop();
-#endif
+
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        SDL_GL_SwapWindow(window);
+        emscripten_sleep(16); // Sleep for ~60 fps
+    }
 
     SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);

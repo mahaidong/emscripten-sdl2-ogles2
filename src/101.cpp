83424f57f20739a6e615d979ffd1bb9f18fa13c4
mahaidong <mahaidong@caad.xyz>
2023-06-28 12:58:34 +0800

    event

--- src/101.cpp
+++ src/101.cpp
@@ -2,6 +2,26 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengles2.h>
 
+bool quit = false;
+
+void mainLoop(void* mainLoopArg) 
+{   
+
+    while (!quit) {
+
+        SDL_Event& event = *((SDL_Event*)mainLoopArg);
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        SDL_GL_SwapWindow(window);
+    }
+
+}
+
 int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("Hello Triangle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
@@ -52,20 +72,11 @@ int main(int argc, char *argv[]) {
     glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE,
                           2 * sizeof(GLfloat), 0);
 
-    bool quit = false;
-    while (!quit) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        SDL_GL_SwapWindow(window);
-        emscripten_sleep(16); // Sleep for ~60 fps
-    }
+    int fps = 0; // Use browser's requestAnimationFrame
+    SDL_Event  event;
+    emscripten_set_main_loop_arg(mainLoop, event, fps, !quit);
 
     SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
